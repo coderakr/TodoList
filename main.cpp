70b6d28c523fd3d2f22c46daffd31b1eb12ad2cf
coderakr <ayushkumar33257@gmail.com>
2025-01-09 09:07:56 +0530

    initialize functions

--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
+#include <fstream>
 #include <iostream>
 using namespace std;
 
-int main (int argc, char *argv[]) {
-    
+void addTask() {}
+void viewTask() {}
+void deleteTask() {}
+
+int main(int argc, char *argv[]) {
     int ch;
-    do{
+    do {
         cout << "1. Add to task" << endl;
         cout << "2. view a task" << endl;
         cout << "3. Delete a task" << endl;
-        cout << "4. Enter your choice" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter your choice : ";
         cin >> ch;
-    }while(ch!=4);
+        cout << endl;
+
+        switch (ch) {
+            case 1:
+                addTask();
+                break;
+            case 2:
+                viewTask();
+                break;
+            case 3:
+                deleteTask();
+                break;
+            default:
+                cout << "Thanks for using this program" << endl;
+                break;
+        }
+    } while (ch != 4);
 
     return 0;
 }
